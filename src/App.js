4864024a567703de4c5d00f8dda70016c21b0ba7
bm4656 <newjin46@gmail.com>
2023-03-08 20:45:36 +0900

    feat: 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Root from './pages/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <p className='bg-red-600'>hi</p>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Header() {
+  return <header className='bg-red-400'>헤더</header>;
+}
