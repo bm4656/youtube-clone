0916eb0ae48d118246ce1d6165b75c31b98796c1
bm4656 <newjin46@gmail.com>
2023-03-09 17:00:50 +0900

    feat: header + search bar 기능

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/YTlogo.png';
+import { GoSearch } from 'react-icons/go';
 
 export default function Header() {
-  return <header className='bg-red-400'>헤더</header>;
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
+  const handleChange = e => {
+    setText(e.target.value);
+  };
+  const handleSearch = () => {
+    console.log(text);
+    navigate(`/${text}`);
+  };
+  return (
+    <header className='bg-slate-500 flex p-2'>
+      <img src={logo} alt='YT_logo' className='h-10 mr-2-2' />
+      <input
+        type='text'
+        value={text}
+        placeholder='검색'
+        onChange={handleChange}
+        className='h-8 rounded-xl'
+      />
+      <button onClick={handleSearch}>
+        <GoSearch />
+      </button>
+    </header>
+  );
 }
