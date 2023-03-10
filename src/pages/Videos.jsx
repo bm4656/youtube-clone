8009af8ab03e81804359c969c75775caab835e4d
bm4656 <newjin46@gmail.com>
2023-03-11 00:38:23 +0900

    feat: video 목록 fetch - mock 데이터(popular)

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], async () => {
+    return fetch(`/data/${keyword ? 'search' : 'popular'}.json`)
+      .then(res => res.json())
+      .then(data => data.items);
+  });
+  console.log(videos);
+  return (
+    <div>
+      Videos {keyword ? `🔎${keyword}` : '🔥'}
+      {isLoading && <p>로딩중...</p>}
+      {error && <p>Something is wrong!!</p>}
+      {videos && (
+        <ul>
+          {videos.map(video => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
