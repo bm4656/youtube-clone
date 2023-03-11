0d62b32fb92ba8fc0d2a4bdf48475338983a8d0d
bm4656 <newjin46@gmail.com>
2023-03-12 01:26:56 +0900

    design: VideoCard 스타일링

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -18,7 +18,7 @@ export default function Videos() {
       {isLoading && <p>로딩중...</p>}
       {error && <p>Something is wrong!!</p>}
       {videos && (
-        <ul>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 gap-y-5'>
           {videos.map(video => (
             <VideoCard key={video.id} video={video} />
           ))}
