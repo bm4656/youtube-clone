fdf20112e0c91de3a5d54cf0dfeb7f3daa14eaa9
bm4656 <newjin46@gmail.com>
2023-03-11 16:18:03 +0900

    feat: api 통신 client별 리팩토링

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,14 @@
 import { createContext, useContext } from 'react';
-import FakeYoutube from '../api/fake-youtube';
+import FakeYoutubeClient from '../api/fakeYoutubeClient';
 import Youtube from '../api/youtube';
+import YoutubeClient from '../api/youtubeClient';
 
 export const YoutubeApiContext = createContext();
 
-// const youtube = new Youtube();
-const youtube = new FakeYoutube();
+const client = new FakeYoutubeClient();
+// const client = new YoutubeClient();
+
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
